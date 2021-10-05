38f61ed797d00ae38cb44c92df868e9377d3531b
Molnár Patrik <lylei.lanar@gmail.com>
2021-10-05 12:51:05 +0200

    Make the main page with blank fields

--- templates/Journeys/index.php
+++ templates/Journeys/index.php
@@ -5,6 +5,34 @@
  */
 ?>
 <div class="index content">
-    <p>Hello world!</p>
+    <div>
+        <h3>My Data</h3>
+        <span>user_name</span>
+        <button>user_settings</button>
+    </div>
+    <div>
+        <h3>Active Journey</h3>
+        <span>j_name</span>
+        <span>start_loc</span>
+        <button>new_jou</button>
+        <button>del_jou</button>
+        <button>mod_jou</button>
+    </div>
+    <div>
+        <h3>Next Destination</h3>
+        <span>trip_name</span>
+        <button>new_trip</button>
+        <button>new_entry</button>
+        <button>mod_trip</button>
+    </div>
+    <div>
+        <h3>Flow</h3>
+        <label for="search-field"></label>
+        <input name="search_field" id="search-field" type="text">
+        <button>Search!</button>
+    </div>
+    <div>
+        <button>refresh</button>
+    </div>
 
 </div>
--- templates/layout/default.php
+++ templates/layout/default.php
@@ -14,17 +14,13 @@
  * @var \App\View\AppView $this
  */
 
-$cakeDescription = 'CakePHP: the rapid development php framework';
 ?>
 <!DOCTYPE html>
-<html>
+<html lang="hu">
 <head>
     <?= $this->Html->charset() ?>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>
-        <?= $cakeDescription ?>:
-        <?= $this->fetch('title') ?>
-    </title>
+    <title>Dice Travel</title>
     <?= $this->Html->meta('icon') ?>
 
     <link href="https://fonts.googleapis.com/css?family=Raleway:400,700" rel="stylesheet">
@@ -36,15 +32,6 @@ $cakeDescription = 'CakePHP: the rapid development php framework';
     <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-nav">
-        <div class="top-nav-title">
-            <a href="<?= $this->Url->build('/') ?>"><span>Cake</span>PHP</a>
-        </div>
-        <div class="top-nav-links">
-            <a target="_blank" rel="noopener" href="https://book.cakephp.org/4/">Documentation</a>
-            <a target="_blank" rel="noopener" href="https://api.cakephp.org/">API</a>
-        </div>
-    </nav>
     <main class="main">
         <div class="container">
             <?= $this->Flash->render() ?>
